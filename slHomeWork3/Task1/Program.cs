f1a57af3fd148acde4248df203378657da65de43
ПК <GIS_1974@tut.by>
2022-07-18 14:15:12 +0300

    Calculate

---
+++ slHomeWork3/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sAll = "";
+            string[] sArray;
+            char[] Sign;
+            do
+            {
+                if (sAll.Length != 0)
+                {
+                    Console.WriteLine("");
+                }
+                try
+                {
+                    Console.WriteLine("input into spase integer numb 1, integer numb 1 and Operation from (+, -, /, *)");
+                    sAll = (Console.ReadLine()).Trim();
+                    sArray = sAll.Split(new char[] { ' ' });
+                    Sign = sArray[2].ToCharArray();
+                    switch (Sign[0])
+                    {
+                        case '-':
+                            Console.WriteLine($"{sArray[0]} {sArray[1]} : {int.Parse(sArray[0]) - int.Parse(sArray[1])}");
+                            break;
+                        case '+':
+                            Console.WriteLine($"{sArray[0]} {sArray[1]} : {int.Parse(sArray[0]) + int.Parse(sArray[1])}");
+                            break;
+                        case '/':
+                            Console.WriteLine($"{sArray[0]} {sArray[1]} : {int.Parse(sArray[0]) / int.Parse(sArray[1])}");
+                            break;
+                        case '*':
+                            Console.WriteLine($"{sArray[0]} {sArray[1]} : {int.Parse(sArray[0]) * int.Parse(sArray[1])}");
+                            break;
+                        default:
+                            Console.WriteLine("Unknow operated!");
+                            break;
+                    }
+                    Console.WriteLine("Для выхода нажмите <Esc>. Else enter any key...");
+                }
+                catch
+                {
+                    Console.WriteLine("String NOT Formating! Enter any key...");
+                }
+            } while ((int)((Console.ReadKey()).Key) != 27);
+        }
+    }
+}
